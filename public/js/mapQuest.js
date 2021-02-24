8f2b87d72de13c7db65e3b950e7eb8e4a396610d
evanklgby <eklam@ucsd.edu>
2021-02-23 23:59:08 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
-	// add your code here
+  L.mapquest.key = 'pjJD4Bol9zsAUs3CGA7mvp1xCXS0s91Z';
+
+  // 'map' refers to a <div> element with the ID map
+  var map = L.mapquest.map('map', {
+    center: [32.87852, -117.23589],
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 12,
+    zoomControl: false,
+  });
+
+  // Add a marker to the map
+  L.marker([32.87852, -117.23589]).addTo(map);
 }
